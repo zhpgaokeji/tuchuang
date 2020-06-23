8a15ce15b1a7d60a4f49e6908b2c72f8cf53a599
zhpgaokeji <67231590+zhpgaokeji@users.noreply.github.com>
2020-06-23 19:56:10 +0800

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,76 @@
+const path = require('path')
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
+module.exports = {
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src/renderer'))
+      .set('~', resolve('src'))
+      .set('root', resolve('./'))
+      .set('#', resolve('src/universal'))
+  },
+  pluginOptions: {
+    electronBuilder: {
+      customFileProtocol: 'picgo://./',
+      externals: ['picgo'],
+      chainWebpackMainProcess: config => {
+        config.resolve.alias
+          .set('@', resolve('src/renderer'))
+          .set('~', resolve('src'))
+          .set('root', resolve('./'))
+          .set('#', resolve('src/universal'))
+          .set('apis', resolve('src/main/apis'))
+          .set('@core', resolve('src/main/apis/core'))
+      },
+      builderOptions: {
+        productName: 'PicGo',
+        appId: 'com.molunerfinn.picgo',
+        publish: [
+          {
+            provider: 'github',
+            owner: 'Molunerfinn',
+            repo: 'PicGo',
+            releaseType: 'draft'
+          }
+        ],
+        dmg: {
+          contents: [
+            {
+              x: 410,
+              y: 150,
+              type: 'link',
+              path: '/Applications'
+            },
+            {
+              x: 130,
+              y: 150,
+              type: 'file'
+            }
+          ]
+        },
+        mac: {
+          icon: 'build/icons/icon.icns',
+          extendInfo: {
+            LSUIElement: 1
+          }
+        },
+        win: {
+          icon: 'build/icons/icon.ico',
+          target: 'nsis'
+        },
+        nsis: {
+          shortcutName: 'PicGo',
+          oneClick: false,
+          allowToChangeInstallationDirectory: true
+        },
+        linux: {
+          icon: 'build/icons/'
+        },
+        snap: {
+          publish: ['github']
+        }
+      }
+    }
+  }
+}
